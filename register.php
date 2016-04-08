1e2cd96505454c20b97cdf5538484225598f7368
Quinn Hosler <qhos93@gmail.com>
2016-04-08 19:35:35 +0000

    inital commit

---
+++ register.php
@@ -0,0 +1,8 @@
+<?php
+
+$REGISTER_LTI2 = array(
+"name" => "iClicker",
+"FontAwesome" => "fa-server",
+"short_name" => "Clicker",
+"description" => "This is a tool that will allow instructors to obtain realtime data about the students understanding of the lecture, as well as easily administer quizzes. This tool is meant to replace premium services such as REEF's i>clicker and PollEverywhere."
+);
