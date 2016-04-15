ff2e84ff216a897ab758dd814bed8c6e59d01326
Quinn Hosler <qhos93@gmail.com>
2016-04-15 19:43:36 +0000

    added livereload to index

--- index.php
+++ index.php
@@ -108,7 +108,10 @@ if ( $USER->instructor ) {
 	include "views/student.php";
 }
 
-
+echo <<<EOT
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
+  ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+EOT;
 
 $OUTPUT->footer();
 
