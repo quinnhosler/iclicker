d9a6be62f6eb184080a72f2a60e4e39df9922e74
Quinn Hosler <qhos93@gmail.com>
2016-04-15 15:05:00 +0000

    removed livereload from single file, added to Tsugi footer

--- views/student.php
+++ views/student.php
@@ -47,10 +47,6 @@ $html = <<< EOT
 </div>
 
 <script>
-	document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
-</script>
-
-<script>
 $("form button.answer").click(function(event) {
 	var target = $(event.target);
 	console.log(target);
