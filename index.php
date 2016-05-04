21589793d49f6ed160c24cf518bf9646148a0ffd
Quinn Hosler <qhos93@gmail.com>
2016-05-04 16:48:56 +0000

    API refinements

--- index.php
+++ index.php
@@ -19,7 +19,12 @@ $old_code = Settings::linkGet('code', '');
 
 if ( $USER->instructor) {
 	
-	if (isset($_POST['type'])) {
+	if (isset($_POST['action']) && strcmp($_POST['action'], "results_window") == 0) {
+		echo(file_get_contents("views/child_window.html"));
+		return;
+	}
+	
+	if (isset($_POST['type']) && strcmp($_POST['type'], "multiple-choice") == 0) {
 
 	//	create poll in DB
 		$title = null;
@@ -86,10 +91,16 @@ if ( $USER->instructor) {
 			}
 		}
 		
+		if (isset($_POST['action']) && strcmp($_POST['action'], "save") == 0) {
+			$stmt = $PDOX->queryDie("UPDATE iclicker_polls SET pending = 1 WHERE poll_id=".$poll_id);
+			echo(json_encode(array("action"=>"save","result"=>true, "poll_id"=>$poll_id, "date"=>date('Y-m-d H:i:s'))));
+			return;
+		}
+		
 	//	process form data, be sure to sanatize input!
 	//	Need to check that owner doesn't have more than one active poll (or do I want to support this?)
 		$stmt = $PDOX->queryDie("INSERT INTO {$p}iclicker_active (poll_id, context_id) VALUES (:PID, :CID) 
-								ON DUPLICATE KEY UPDATE poll_id=:ID", 
+								ON DUPLICATE KEY UPDATE poll_id=:PID", 
 		array(
 			':PID' => $poll_id,
 			':CID' => $CONTEXT->id
@@ -117,7 +128,7 @@ if ( $USER->instructor) {
 
 		$socket->send(json_encode($entryData));
 		
-		echo(json_encode(array('poll_id'=>$poll_id, 'date'=>time())));
+		echo(json_encode(array('poll_id'=>$poll_id, 'date'=>date('Y-m-d H:i:s'))));
 		return;
 	}
 	
@@ -141,29 +152,42 @@ if ( $USER->instructor) {
 	else if (isset($_POST['action']) && strcmp($_POST['action'],"retract_poll") == 0) {
 		if (!isset($_POST['poll_id'])) { echo("incomplete API call; missing parameter"); return; }
 		
-		$PDOX->queryDie("UPDATE {$p} iclicker_polls SET completed=NOW() WHERE poll_id=".$_POST['poll_id']);
+		$PDOX->queryDie("UPDATE iclicker_polls SET completed=NOW() WHERE poll_id=".$_POST['poll_id']);
 		$stmt = $PDOX->queryDie("DELETE FROM {$p}iclicker_active WHERE context_id=".$CONTEXT->id);
 		if ($stmt->rowCount() > 0) {
-			echo("true"); return;
+			echo("true");
 		} else {
-			echo('no poll to retract'); return;
+			echo('no poll to retract');
 		}
+		
+		$entryData = array(
+				'category' => 'retract',
+				'when'     => time()
+		);
+		
+		$context = new ZMQContext();
+		$socket = $context->getSocket(ZMQ::SOCKET_PUSH, 'my pusher');
+		$socket->connect("tcp://localhost:5555");
+
+		$socket->send(json_encode($entryData));
+		
+		return;
 	}
 	
 	else if (isset($_POST['action']) && strcmp($_POST['action'],"get_archive") == 0) {
 		if (isset($_POST['range']) && $_POST['range'] != "undefined") {
 			$stmt = $PDOX->queryDie("SELECT p.poll_id, p.title, p.completed AS date, COUNT(r.user_id) AS count, SUM(r.choice_id=p.answer_id) AS correct 
 									FROM {$p}iclicker_polls p LEFT JOIN {$p}iclicker_responses r 
-									ON p.poll_id=r.poll_id WHERE context_id=:CID 
+									ON p.poll_id=r.poll_id WHERE context_id=:CID AND p.pending IS NULL
 									GROUP BY p.poll_id ORDER BY modified 
 									DESC LIMIT ".$_POST['range'].", 5", array(':CID'=>$CONTEXT->id));
-									$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
 			echo(json_encode($row));
 		} else {
 			$stmt = $PDOX->queryDie("SELECT p.poll_id, p.title, p.completed AS date, COUNT(r.user_id) AS count, SUM(r.choice_id=p.answer_id) AS correct 
 									FROM {$p}iclicker_polls p LEFT JOIN {$p}iclicker_responses r 
-									ON p.poll_id=r.poll_id WHERE context_id=:CID 
-									GROUP BY p.poll_id ORDER BY modified 
+									ON p.poll_id=r.poll_id WHERE context_id=:CID AND p.pending IS NULL
+									GROUP BY p.poll_id ORDER BY modified  
 									DESC LIMIT 5", array(':CID'=>$CONTEXT->id));
 			$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
 			echo(json_encode($row));
@@ -187,6 +211,89 @@ if ( $USER->instructor) {
 		$row['responses'] = $responses;
 		$row['choices'] = $choices;
 		echo(json_encode($row));
+		return;
+	}
+	
+	else if (isset($_POST['action']) && strcmp($_POST['action'],"copy_poll") == 0) {
+		if (!isset($_POST['poll_id'])) { echo("incomplete API call; missing parameter"); return; }
+		
+		$stmt = $PDOX->queryDie("SELECT poll_id, title as ':t', answer_id as ':a', ordered as ':o' FROM iclicker_polls WHERE poll_id=".$_POST['poll_id']);
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		
+		$old_poll_id = $row['poll_id'];
+		unset($row['poll_id']);
+		$row[':CID'] = $CONTEXT->id;
+		if (strcmp($row[':t'], "") == 0)
+			$row[':t'] = null;
+		
+		$stmt = $PDOX->queryDie("INSERT INTO iclicker_polls (title, context_id, answer_id, ordered, modified, pending) VALUES ( :t, :CID, :a, :o, NOW(), 1)", $row);
+		$new_poll_id = $PDOX->lastInsertId();
+		
+		$stmt = $PDOX->queryDie("SELECT choice_id FROM iclicker_pollchoices WHERE poll_id=".$old_poll_id);
+		$choices = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		
+		foreach ($choices as $choice) {
+			$stmt = $PDOX->queryDie("INSERT INTO iclicker_pollchoices (poll_id, choice_id) VALUES (:p, :c)", array(':p'=>$new_poll_id,':c'=>$choice['choice_id']));
+		}
+		
+		echo(json_encode(array('poll_id'=>$new_poll_id)));
+		return;
+	}
+	
+	else if (isset($_POST['action']) && strcmp($_POST['action'],"get_pending") == 0) {
+		$stmt = $PDOX->queryDie("SELECT poll_id,title,modified as date FROM iclicker_polls WHERE context_id=:CID AND pending=1",array(":CID"=>$CONTEXT->id));
+		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		$response = array("action"=>"get_pending", "result"=>$rows);
+		echo(json_encode($response));
+		return;
+	}
+	
+	else if (isset($_POST['action']) && strcmp($_POST['action'],"make_active") == 0) {
+		if (!isset($_POST['poll_id'])) { echo("incomplete API call; missing parameter"); return; }
+		
+		$stmt = $PDOX->queryDie("INSERT INTO {$p}iclicker_active (poll_id, context_id) VALUES (:PID, :CID) 
+								ON DUPLICATE KEY UPDATE poll_id=:PID", 
+		array(
+			':PID' => $_POST['poll_id'],
+			':CID' => $CONTEXT->id
+			)
+		);
+		
+		$PDOX->queryDie("UPDATE iclicker_polls SET pending=NULL WHERE poll_id=".$_POST['poll_id']);
+		$stmt = $PDOX->queryDie("SELECT c.choice_id, c.choice_value FROM iclicker_pollchoices pc
+								JOIN iclicker_choices c ON pc.choice_id=c.choice_id
+								WHERE pc.poll_id=".$_POST['poll_id']);
+		$choices = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		
+		$choice_ids = array();
+		foreach ($choices as $choice) {
+			$choice_ids[$choice['choice_id']] = $choice['choice_value'];
+		}
+		
+		$entryData = array(
+				'category' => 'polls',
+				'type' => "multiple-choice",
+				'poll' => $_POST['poll_id'],
+				'choices'  => $choice_ids,
+				'order' => 'random',
+				'when'     => time()
+		);
+		
+		$context = new ZMQContext();
+		$socket = $context->getSocket(ZMQ::SOCKET_PUSH, 'my pusher');
+		$socket->connect("tcp://localhost:5555");
+
+		$socket->send(json_encode($entryData));
+		echo(json_encode($entryData));
+		return;
+	}
+	
+	else if (isset($_POST['action']) && strcmp($_POST['action'],"delete_poll") == 0) {
+		if (!isset($_POST['poll_id'])) { echo("incomplete API call; missing parameter"); return; }
+		
+		$PDOX->queryDie("DELETE FROM iclicker_polls WHERE poll_id=".$_POST['poll_id']." AND context_id=".$CONTEXT->id);
+		$PDOX->queryDie("DELETE FROM iclicker_pollchoices WHERE poll_id=".$_POST['poll_id']);
+		echo('true');
 		return;
 	}
 	
@@ -276,68 +383,8 @@ if ( $USER->instructor) {
 		echo(json_encode($row));
 		return;
 	}
-	
-	
-	
-
-//    if ( $old_code == $_POST['code'] ) {
-//        $PDOX->queryDie("INSERT INTO {$p}attend
-//            (link_id, user_id, ipaddr, attend, updated_at)
-//            VALUES ( :LI, :UI, :IP, NOW(), NOW() )
-//            ON DUPLICATE KEY UPDATE updated_at = NOW()",
-//            array(
-//                ':LI' => $LINK->id,
-//                ':UI' => $USER->id,
-//                ':IP' => $_SERVER["REMOTE_ADDR"]
-//            )
-//        );
-//        $_SESSION['success'] = __('Attendance Recorded...');
-//    } else {
-//        $_SESSION['error'] = __('Code incorrect');
-//    }
-//    header( 'Location: '.addSession('index.php') ) ;
-//    return;
 }
-//
-//// View
-//$OUTPUT->header();
-//$OUTPUT->bodyStart();
-//$OUTPUT->flashMessages();
-//$OUTPUT->welcomeUserCourse();
-//
-//// We could use the settings form - but we will keep this simple
-//echo('<form method="post">');
-//echo(_("Enter code:")."\n");
-//if ( $USER->instructor ) {
-//    echo('<input type="text" name="code" value="'.htmlent_utf8($old_code).'"> ');
-//    echo('<input type="submit" class="btn btn-normal" name="set" value="'._('Update code').'"> ');
-//    echo('<input type="submit" class="btn btn-warning" name="clear" value="'._('Clear data').'"><br/>');
-//} else {
-//    echo('<input type="text" name="code" value=""> ');
-//    echo('<input type="submit" class="btn btn-normal" name="set" value="'._('Record attendance').'"><br/>');
-//}
-//echo("\n</form>\n");
-//
-//if ( $USER->instructor ) {
-//    $rows = $PDOX->allRowsDie("SELECT user_id,attend,ipaddr FROM {$p}attend
-//            WHERE link_id = :LI ORDER BY attend DESC, user_id",
-//            array(':LI' => $LINK->id)
-//    );
-//    echo('<table border="1">'."\n");
-//    echo("<tr><th>"._("User")."</th><th>"._("Attendance")."</th><th>"._("IP Address")."</th></tr>\n");
-//    foreach ( $rows as $row ) {
-//        echo "<tr><td>";
-//        echo($row['user_id']);
-//        echo("</td><td>");
-//        echo($row['attend']);
-//        echo("</td><td>");
-//        echo(htmlent_utf8($row['ipaddr']));
-//        echo("</td></tr>\n");
-//    }
-//    echo("</table>\n");
-//}
 
-//$OUTPUT->footer();
 
 // View
 $OUTPUT->header();
